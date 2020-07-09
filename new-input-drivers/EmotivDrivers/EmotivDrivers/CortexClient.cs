d12bb0c51a8e83de31c884ba36afcddc763c7154
Emil Nilsen <emil.nilsen9888@gmail.com>
2020-07-09 14:52:50 +0200

    Add global variables

--- new-input-drivers/EmotivDrivers/EmotivDrivers/CortexClient.cs
+++ new-input-drivers/EmotivDrivers/EmotivDrivers/CortexClient.cs
@@ -1,15 +1,34 @@
 ﻿using System;
-using WebSocketSharp;
+using System.Collections.Generic;
+using System.Threading;
+using WebSocket4Net;
 using Newtonsoft.Json.Linq;
 
 namespace EmotivDrivers {
     
-    public class CortexClient {
+    public sealed class CortexClient {
 
         private const string CortexURL = "wss://localhost:6868";
-        private const string clientId = "giK2jIkOy5x0Ry1xwixpdbAykYZi1Ebr3xjv7Asy";
-        private const string clientSecret =
+        private const string ClientId = "giK2jIkOy5x0Ry1xwixpdbAykYZi1Ebr3xjv7Asy";
+        private const string ClientSecret =
             "SzXU4drfTJuAshSb9wInyvE3MYx5Z0jAZR1Au0b2ETXp0F8T7wpbTYXzAqezvJxYM7u9UJndXkiKdUDD6hE5h0G5ZbEQdtQcCn43PJXzme9DHsS95alVLQtCDGgql4Ot";
+
+        private string CurrentMessage = string.Empty;
+        private Dictionary<int, string> MethodForRequestID;
+
+        private WebSocket WebSocketClient;
+        private int NextRequestId;
+        private bool IsWebSocketClientConnected;
         
+        // Events
+        private AutoResetEvent MessageReceivedEvent = new AutoResetEvent(false);
+        private AutoResetEvent OpenedEvent = new AutoResetEvent(false);
+        private AutoResetEvent CloseEvent = new AutoResetEvent(false);
+        
+        static CortexClient() {}
+
+        private CortexClient() {
+            
+        }
     }
 }
