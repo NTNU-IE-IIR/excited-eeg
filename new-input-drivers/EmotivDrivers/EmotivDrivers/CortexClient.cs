5957e3fbfb5c6faea80344cef61799a5f8750d6f
Emil Nilsen <emil.nilsen9888@gmail.com>
2020-07-10 11:48:50 +0200

    Add config class

--- new-input-drivers/EmotivDrivers/EmotivDrivers/CortexClient.cs
+++ new-input-drivers/EmotivDrivers/EmotivDrivers/CortexClient.cs
@@ -34,9 +34,6 @@ namespace EmotivDrivers {
     public sealed class CortexClient {
 
         private const string CortexURL = "wss://localhost:6868";
-        private const string ClientId = "giK2jIkOy5x0Ry1xwixpdbAykYZi1Ebr3xjv7Asy";
-        private const string ClientSecret =
-            "SzXU4drfTJuAshSb9wInyvE3MYx5Z0jAZR1Au0b2ETXp0F8T7wpbTYXzAqezvJxYM7u9UJndXkiKdUDD6hE5h0G5ZbEQdtQcCn43PJXzme9DHsS95alVLQtCDGgql4Ot";
 
         private string currentMessage = string.Empty;
         private Dictionary<int, string> methodForRequestID;
