c8e85cb6d8de35bba042c308ef17a28403c1f55a
Emil Nilsen <emil.nilsen9888@gmail.com>
2020-07-13 16:51:19 +0200

    Start writing training code

---
+++ new-input-drivers/EmotivDrivers/EmotivDrivers/CortexClient/Training.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace EmotivDrivers.CortexClient {
+    public class Training {
+
+        private CortexClient cortexClient;
+        private string profileName;
+        private string cortexToken;
+        private string sessionId;
+        private string detection;
+        private bool isProfileLoaded;
+        private string headsetId;
+        private List<string> availableActions;
+
+        private HeadsetFinder headsetFinder;
+        private Authorizer authorizer;
+        private SessionCreator sessionCreator;
+        private List<string> profileList;
+
+        public Training() {
+            authorizer = new Authorizer();
+            headsetFinder = new HeadsetFinder();
+            sessionCreator = new SessionCreator();
+            cortexToken = "";
+            sessionId = "";
+            isProfileLoaded = false;
+            availableActions = new List<string>();
+            profileList = new List<string>();
+            
+            cortexClient = CortexClient.Instance;
+        }
+    }
+}
