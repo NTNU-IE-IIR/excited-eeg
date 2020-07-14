350fe5bfac569d5dda6d3e4f9b9eac02f1b00b94
Emil Nilsen <emil.nilsen9888@gmail.com>
2020-07-14 11:54:11 +0200

    Add utilities

---
+++ new-input-drivers/EmotivDrivers/EmotivDrivers/CortexClient/Utils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EmotivDrivers.CortexClient {
+    public class Utils {
+
+        public static Int64 GetEpochTimeNow() {
+            TimeSpan time = DateTime.UtcNow - new DateTime(1970, 1, 1);
+            Int64 timeSinceEpoch = (Int64) time.TotalMilliseconds;
+            return timeSinceEpoch;
+        }
+
+        public static string GenerateUuidProfileName(string prefix) {
+            return prefix + "-" + GetEpochTimeNow();
+        }
+    }
+}
