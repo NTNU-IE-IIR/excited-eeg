3ef2469eeb1c925393e8a3cd01aeda214ccec762
Emil Nilsen <emil.nilsen9888@gmail.com>
2020-08-07 09:50:17 +0200

    Add stop drivers button

--- new-input-drivers/EmotivDrivers/EmotivDrivers/GUI/ConsoleOutputGUI.cs
+++ new-input-drivers/EmotivDrivers/EmotivDrivers/GUI/ConsoleOutputGUI.cs
@@ -7,21 +7,29 @@ namespace EmotivDrivers.GUI {
     public class ConsoleOutputGUI : GUI {
         
         private TextBox consoleOutputTextBox;
-        private int consoleOutputTextBoxWidth = 750;
-        private int consoleOutputTextBoxHeight = 400;
-
-        private string consoleOutputtextBoxValue;
         public TextBox ConsoleOutputTextBox {
             get => consoleOutputTextBox;
             set => consoleOutputTextBox = value;
         }
         
+        private int consoleOutputTextBoxWidth = 750;
+        private int consoleOutputTextBoxHeight = 400;
+
+        private Button stopDriverButton;
+        
         public ConsoleOutputGUI() {
             InitComponents();
+            
+            SubscribeToEvents();
+        }
+
+        private void SubscribeToEvents() {
+            stopDriverButton.Click += new EventHandler(OnStopDriverButtonClick);
         }
         
         private void InitComponents() {
             this.consoleOutputTextBox = new TextBox();
+            this.stopDriverButton = new Button();
 
             this.Location = this.Location;
             this.StartPosition = FormStartPosition.Manual;
@@ -30,7 +38,8 @@ namespace EmotivDrivers.GUI {
 
             this.Text = "Emotiv driver output";
             
-            InitConsoleOutputComponents();
+            SetupConsoleOutputComponents();
+            SetupStopDriverButton();
             
             // Set application icon
             using (var stream = File.OpenRead("Resources/ntnu.ico")) {
@@ -43,17 +52,27 @@ namespace EmotivDrivers.GUI {
                 Console.SetOut(consoleWriter);
             }
             
-            
             this.Controls.Add(consoleOutputTextBox);
+            this.Controls.Add(stopDriverButton);
         }
         
-        private void InitConsoleOutputComponents() {
+        private void SetupConsoleOutputComponents() {
             this.consoleOutputTextBox.ReadOnly = true;
-            this.consoleOutputTextBox.Location = new Point((guiWidth / 2) - (consoleOutputTextBoxWidth / 2), (guiHeight / 2) - consoleOutputTextBoxHeight / 2);
+            this.consoleOutputTextBox.Location = new Point((guiWidth / 2) - (consoleOutputTextBoxWidth / 2), (guiHeight / 2) - consoleOutputTextBoxHeight / 2 - 20);
             this.consoleOutputTextBox.MinimumSize = new Size(consoleOutputTextBoxWidth, consoleOutputTextBoxHeight);
             this.consoleOutputTextBox.Font = new Font("", 12);
             this.consoleOutputTextBox.Multiline = true;
             this.consoleOutputTextBox.ScrollBars = ScrollBars.Both;
+        }
+
+        private void SetupStopDriverButton() {
+            this.stopDriverButton.Text = "Stop Drivers";
+            this.stopDriverButton.ForeColor = Color.White;
+            this.stopDriverButton.AutoSize = true;
+            this.stopDriverButton.TextAlign = ContentAlignment.MiddleCenter;
+            this.stopDriverButton.BackColor = Color.FromArgb(255, 235, 26, 26);
+            this.stopDriverButton.Font = new Font("Verdana", 14);
+            this.stopDriverButton.Location = new Point((guiWidth / 2) - stopDriverButton.Size.Width, (guiHeight / 2) + 185);
         }
         
         private void ConsoleWriterWriteEvent(object sender, ConsoleWriterEventArgs eventArgs) {
@@ -76,6 +95,10 @@ namespace EmotivDrivers.GUI {
                 Console.WriteLine(e);
                 throw;
             }
+        }
+
+        private void OnStopDriverButtonClick(object sender, EventArgs eventArgs) {
+            Application.Exit();
         }
 
         protected override void OnFormClosing(FormClosingEventArgs e) {
