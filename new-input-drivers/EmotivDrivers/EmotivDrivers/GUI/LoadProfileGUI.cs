81b79e1b94a26d3d0e875a5d8358c3fbfbb3a62b
Emil <emil.nilsen9888@gmail.com>
2020-08-12 09:45:30 +0200

    Button layout now looks good

--- new-input-drivers/EmotivDrivers/EmotivDrivers/GUI/LoadProfileGUI.cs
+++ new-input-drivers/EmotivDrivers/EmotivDrivers/GUI/LoadProfileGUI.cs
@@ -25,22 +25,32 @@ namespace EmotivDrivers.GUI {
             Button button1 = new Button();
             Button button2 = new Button();
             Button button3 = new Button();
+            Button button4 = new Button();
+            Button button5 = new Button();
             
             button1.Size = new Size(200, 100);
             button2.Size = new Size(200, 100);
             button3.Size = new Size(200, 100);
+            button4.Size = new Size(200, 100);
+            button5.Size = new Size(200, 100);
 
             button2.Location = new Point(0, 110);
             button3.Location = new Point(0, 220);
+            button4.Location = new Point(0, 330);
+            button5.Location = new Point(0, 440);
             
             buttonContainer.AutoSize = true;
 
             buttonContainer.Controls.Add(button1);
             buttonContainer.Controls.Add(button2);
             buttonContainer.Controls.Add(button3);
+            buttonContainer.Controls.Add(button4);
+            buttonContainer.Controls.Add(button5);
             
             buttonContainer.Location = new Point(guiWidth / 2 - buttonContainer.Size.Width / 2, 10);
-            
+
+            this.AutoScroll = true;
+
             this.Controls.Add(buttonContainer);
         }
     }
